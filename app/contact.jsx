8f9feb1b98ed11828f2863c33b6d968111295927
Nana Mohamadi <adilnana78@gmail.com>
2025-04-12 17:09:27 +0000

    Mises en page et pile

--- app/contact.jsx
+++ app/contact.jsx
@@ -1,32 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 const Contact = () => {
   return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Contact page</Text>
-        <Link href="/" style={styles.link}>
-          Go Back Home
-        </Link>
-      </View>
-    );
-}
+    <View style={styles.container}>
+      <Text style={styles.title}>Contact page</Text>
+      <Link href="/" style={styles.link}>
+        Go Back Home
+      </Link>
+    </View>
+  );
+};
 
-export default Contact
+export default Contact;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-    },
-    link: {
-      marginVertical: 10,
-      borderBottomWidth: 1,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+});
